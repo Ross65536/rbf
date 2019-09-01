fe2e0ae357b5bc90866eedc67044248e4f490391
ros <rk65536@protonmail.com>
2019-09-01 19:24:26 +0100

    add regex crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+
+use std::io::Read;
+
+enum Token {
+    IncCell(i64),
+    IncPointer(i64),
+
+}
+
+fn parse_tokens(reader: &mut Read) -> Vec<Token> {
+    let tokens = vec![];
+    let mut contents = String::new();
+    
+    reader.read_to_string(&mut contents)
+        .unwrap();
+
+    contents.chars().filter(|c| true);
+    
+    tokens
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
+
+
