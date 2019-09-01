a1d7f6868fcbc8346a66367131e5d5ab182434bb
ros <rk65536@protonmail.com>
2019-09-01 20:24:15 +0100

    implement token parsing

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,71 @@
-
 use std::io::Read;
+use std::io::stdin;
 
+#[derive(Debug)]
 enum Token {
-    IncCell(i64),
-    IncPointer(i64),
-
+  IncCell(i64),
+  IncPointer(i64),
+  ReadInput,
+  Print,
+  StartLoop,
+  EndLoop
 }
 
-fn parse_tokens(reader: &mut Read) -> Vec<Token> {
-    let tokens = vec![];
-    let mut contents = String::new();
-    
-    reader.read_to_string(&mut contents)
-        .unwrap();
+fn is_token_char(c: char) -> bool {
+  match c {
+    ',' | '.' | '[' | ']' | '+' | '-' | '>' | '<' => true,
+    _ => false,
+  }
+}
 
-    contents.chars().filter(|c| true);
-    
-    tokens
+fn char_to_token(c: char) -> Token {
+  match c {
+    ',' => Token::ReadInput,
+    '.' => Token::Print,
+    '[' => Token::StartLoop,
+    ']' => Token::EndLoop,
+    '+' => Token::IncCell(1),
+    '-' => Token::IncCell(-1),
+    '>' => Token::IncPointer(1),
+    '<' => Token::IncPointer(-1),
+    _ => panic!("invalid usage")
+  }
+}
 
+fn parse_tokens(reader: &mut dyn Read) -> Vec<Token> {
+  let mut contents = String::new();
+  
+  reader.read_to_string(&mut contents)
+      .unwrap();
+
+  contents.chars()
+    .filter(|c| is_token_char(*c))
+    .map(|c| char_to_token(c))
+    .collect()
+}
+
+fn validate(tokens: &Vec<Token>) {
+  let mut loop_start_counter = 0;
+
+  for token in tokens {
+    loop_start_counter += match token {
+      Token::StartLoop => 1,
+      Token::EndLoop => -1,
+      _ => 0
+    };
+
+    if loop_start_counter < 0 {
+      panic!("Mismatched brackets");
+    }
+  }
 }
 
 fn main() {
-    println!("Hello, world!");
+  let tokens = parse_tokens(&mut stdin());
+
+  validate(&tokens);
+
+  println!("{:?}", tokens);
 }
 
 
