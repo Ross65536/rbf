161c961ce617da47f5c88e59fbd528ae02ed8450
ros <rk65536@protonmail.com>
2019-09-03 17:54:49 +0100

    fix input parsing

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,24 @@ use std::io::Read;
 use std::fs::File;
 mod injection;
 
-pub fn testable_main(stdin: &mut impl Read, stdout: &mut impl Write) {
+// TODO fix clone hack
+pub fn testable_main(stdin: &mut impl Read, stdin_clone: &mut impl Read, stdout: &mut impl Write) {
   let (commands, _flags) = parse(env::args());
-  let mut source : Box<dyn Read> = if commands.len() >= 1 {
-    Box::new(File::open(commands[0].clone()).unwrap())
+  let (mut source, mut stdin) : (Box<dyn Read>, Box<dyn Read>) = if commands.len() >= 1 {
+    (Box::new(File::open(commands[0].clone()).unwrap()), Box::new(stdin))
   } else {
-    Box::new(stdin)
+    (Box::new(stdin), Box::new(stdin_clone))
   };
 
   let tokens = parser::parse_tokens(&mut source);
   parser::validate(&tokens);
   let mut interpreter = Interpreter::new();
-  interpreter.execute(tokens, &mut source, stdout);
+  interpreter.execute(tokens, &mut stdin, stdout);
 }
 
+
 fn main() {
-  testable_main(&mut stdin(), &mut stdout())
+  testable_main(&mut stdin(), &mut stdin(), &mut stdout())
 }
 
 
